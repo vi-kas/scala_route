3583aaaafa7a79f6bdf16dfb3e26089d9cb3bde0
Vikash Sharma <vika.sharma@live.com>
2020-03-13 23:02:30 +0800

    simple-akka-http 1

--- simple-akka-http/usage/src/main/scala/io/github/vi_kas/routes/OrderRoutes.scala
+++ simple-akka-http/usage/src/main/scala/io/github/vi_kas/routes/OrderRoutes.scala
@@ -12,41 +12,43 @@ class OrderRoutes(orderService: OrderService) {
 
   def config: Config = ConfigFactory.load()
 
+  /* Akka Http Routes
+   * ----------------
+   * Use of `concat` here:
+   * Tries the supplied routes in sequence, returning the result of the first route that doesn't reject the request.
+   *
+   * Eg.
+   * In the case of:
+   *    GET /orders - `get(complete(StatusCodes.OK, s"GET All Orders request."))` will be executed.
+   *
+   *    GET /orders/<some uuid> - `get(complete(StatusCodes.OK, s"GET request for id: $id"))` will be executed.
+   *    GET /orders/<uuid>/price - `(get & path("price"))(complete(StatusCodes.OK, s"Price: DummyPriceValue for id: $id"))`
+   *
+   *    GET /orders/count - `(get & path("count"))(complete(StatusCodes.OK, "Count: DummyCountNumber"))` will be executed.
+   */
   val routes: Route =
-    pathPrefix("orders") {
-
-      pathEndOrSingleSlash {
-
-        //POST /orders
-        (post & entity(as[Order])) { order =>
-          respond(orderService.create(order))
-        } ~
-          get {
-            //GET /orders
-            respond(orderService.all)
-          }
-      } ~
-        {
-          path(JavaUUID) {id =>
-
+    pathPrefix("orders"){
+      concat(
+        pathEndOrSingleSlash {
+          concat(
+            get(respond(orderService.all)),
+            (post & entity(as[Order]))(order => respond(orderService.create(order)))
+          )
+        },
+        pathPrefix(JavaUUID){ id =>
+          concat(
             pathEndOrSingleSlash {
-
-              //GET /orders/someUUID
-              get {
-                respond(orderService.byId(id))
-              } ~
-                (put & entity(as[Order])){ _ =>     //PUT /orders/someUUID
-
-                  complete(StatusCodes.Accepted)
-                } ~
-                delete {
-                  //DELETE /orders/someUUID
-                  respond(orderService.delete(id))
-                }
-            }
-          }
-        }
-
+              concat(
+                get(respond(orderService.byId(id))),
+                delete(respond(orderService.delete(id))),
+                (put & entity(as[Order]))(_ => complete(StatusCodes.Accepted))
+              )
+            },
+            (get & path("price"))(complete(StatusCodes.OK, "Price: DummyPriceValue"))
+          )
+        },
+        (get & path("count"))(complete(StatusCodes.OK, "Count: DummyCountNumber"))
+      )
     }
 
 }
