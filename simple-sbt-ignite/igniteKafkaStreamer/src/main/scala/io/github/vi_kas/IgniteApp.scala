253306e7d4022285820e7bec3e0cbaffb0e1d2cd
Vikash Sharma <vika.sharma@live.com>
2020-03-26 22:52:26 +0800

    .

--- simple-sbt-ignite/igniteKafkaStreamer/src/main/scala/io/github/vi_kas/IgniteApp.scala
+++ simple-sbt-ignite/igniteKafkaStreamer/src/main/scala/io/github/vi_kas/IgniteApp.scala
@@ -1,16 +1,18 @@
 package io.github.vi_kas
 
 import java.util
-import java.util.Properties
+import java.util.{AbstractMap, Properties}
 
-import org.apache.ignite.Ignition
+import org.apache.ignite.{Ignite, IgniteCache, IgniteDataStreamer, Ignition}
 import org.apache.ignite.configuration.{CacheConfiguration, IgniteConfiguration}
-import org.apache.ignite.stream.StreamMultipleTupleExtractor
 import org.apache.ignite.stream.kafka.KafkaStreamer
 import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.connect.sink.SinkRecord
 import org.slf4j.{Logger, LoggerFactory}
+
+
+// https://github.com/apache/ignite/blob/master/modules/kafka/src/test/java/org/apache/ignite/stream/kafka/KafkaIgniteStreamerSelfTest.java
 
 object IgniteApp {
 
@@ -42,22 +44,28 @@ object IgniteApp {
     val igniteConfiguration: IgniteConfiguration =
       new IgniteConfiguration().setCacheConfiguration(simpleCacheConfiguration)
 
-    val ignite = Ignition.start(igniteConfiguration)
+    val ignite: Ignite = Ignition.start(igniteConfiguration)
     logger.info(s"Ignite cache started with config: $igniteConfiguration")
 
-    val igniteStreamer = ignite.dataStreamer("simple-ignite-kafka-cache")
+    val igniteStreamer: IgniteDataStreamer[String, String] = ignite.dataStreamer("simple-ignite-kafka-cache")
     igniteStreamer.allowOverwrite(true)
     logger.info(s"IgniteStreamer created: $igniteStreamer")
 
     kafkaStreamer.setIgnite(ignite)
     kafkaStreamer.setTopic(topics)
     kafkaStreamer.setThreads(2)
-//    kafkaStreamer.setSingleTupleExtractor(new DefaultSingleTupleStreamExtractor)
+    kafkaStreamer.setStreamer(igniteStreamer)
+    kafkaStreamer.setSingleTupleExtractor(new DefaultSingleTupleStreamExtractor)
+
+    val cache: IgniteCache[String, String] = ignite.cache("simple-ignite-kafka-cache")
 
     logger.info(s"Starting kafkaStreamer ...")
     kafkaStreamer.start()
 
-    Thread.sleep(10)
+    cache.get("key1")
+
+    logger.info(s"Waiting for 10 seconds!")
+    Thread.sleep(10000)
 
     logger.info(s"Stopping kafkaStreamer ...")
     kafkaStreamer.stop()
